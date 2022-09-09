1b57f7ca7ffaa473e3654f0bc689e2a538c4b421
ITORUM\a.shakov <a.shakov@itorum.ru>
2022-09-09 17:56:09 +0300

    first commit

---
+++ testing/main/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserAccountManager(BaseUserManager):
+    use_in_migrations = True
+
+    # "_" means that this function works only internally, an ordinary client must not know about that
+    def _create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError('Email address must be provided')
+ 
+        if not password:
+            raise ValueError('Password must be provided')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user        
+
+    def create_user(self, email=None, password=None, **kwargs):
+        return self._create_user(email, password, **kwargs)
+    
+    def create_superuser(self, email, password, **kwargs):
+        kwargs['is_staff'] = True
+        kwargs['is_superuser'] = True
+ 
+        return self._create_user(email, password, **kwargs)  
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email' # USERNAME_FIELD means that authentication will require this field(email)
+
+    # We have to define it, the documentation says so
+    objects = UserAccountManager()
+
+    email = models.EmailField('email', unique=True, blank=False, null=False)
+    full_name = models.CharField('full name', blank=True, null=True, max_length=400)
+    is_staff = models.BooleanField('staff status', default=False)
+    is_active = models.BooleanField('active', default=True)
+
+    def get_short_name(self):
+        return self.email
+ 
+    def get_full_name(self):
+        return self.email
+ 
+    def __unicode__(self):
+        return self.email
+
+
+
+
---
+++ testing/publish/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+# https://code.tutsplus.com/ru/tutorials/using-celery-with-django-for-background-task-processing--cms-28732
+class Post(models.Model):
+    # get_user_model gets the currents user. So if a user has signed in we can get him/her by using get_user_model()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField('Created Date', default=timezone.now)
+    title = models.CharField('Title', max_length=200)
+    content = models.TextField('Content')
+    slug = models.SlugField('Slug')
+ 
+    def __str__(self):
+        return '"%s" by %s' % (self.title, self.author)
